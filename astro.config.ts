520fbe5846baacb53f57586ec244aafb0b1fa6d2
Aurélien Chauveau <aure.chv49@gmail.com>
2025-03-18 16:24:53 +0100

    changement url

--- astro.config.ts
+++ astro.config.ts
@@ -5,10 +5,7 @@ import { defineConfig } from 'astro/config'
 import UnoCSS from 'unocss/astro'
 
 export default defineConfig({
-  site: 'https://astro-theme-vitesse.netlify.app/',
-  server: {
-    port: 1977,
-  },
+  site: 'https://portfolio-ac-2025.vercel.app',
   integrations: [
     mdx(),
     sitemap(),
